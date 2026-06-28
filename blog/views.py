0c43cc979c8ac4a2b6b23d2fa89707b73e8c0cc8
luciano <luc02@live.com>
2026-06-28 10:35:13 -0300

    Fix para mostrar imagen

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,19 @@
+import base64
 from django.shortcuts import render, get_object_or_404
 from .models import Perfil, Post
+
 
 def index(request):
     perfil = Perfil.objects.first()
     posts = Post.objects.all()
+
+    foto_base64 = None
+    if perfil and perfil.foto:
+        with open(perfil.foto.path, 'rb') as f:
+            foto_base64 = base64.b64encode(f.read()).decode('utf-8')
+
     return render(request, 'blog/index.html', {
         'perfil': perfil,
         'posts': posts,
-    })
-
-def post_detalle(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    perfil = Perfil.objects.first()
-    return render(request, 'blog/post.html', {
-        'post': post,
-        'perfil': perfil,
+        'foto_base64': foto_base64,
     })
